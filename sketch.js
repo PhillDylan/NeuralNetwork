41160394a6bed38b45919e1bcfddb42bdf5b15b3
PhillDylan <dylan.pdcs@gmail.com>
2023-08-25 14:44:25 -0300

    Feat: adiçao de comentarios

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
+// Função de configuração executada uma vez quando o programa é iniciado
 function setup(){
+    // Criando um canvas de 500x500 pixels
     createCanvas(500,500);
+    // Definindo a cor de fundo do canvas como preto
     background(0);
 
+    // Criando uma nova instância da classe Matrix com 3 linhas e 3 colunas
     var m = new Matrix(3,3);
+    
+    // Exibindo a matriz no console
     console.log(m)
-
-
 }
 
+// Função de desenho executada continuamente em um loop
 function draw(){
 
 }
